4b0b1eed27e630ab6c11fa18e560f6b7a4a5966c
saintilien1 <saintilien49@yahoo.com>
2021-03-30 21:34:00 -0400

    adding the last thing

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var app4 = new Vue({
     todos: [
       { text: 'Learn JavaScript' },
       { text: 'Learn Vue' },
+      { text: 'Learn Bonjour' },
       { text: 'Build something awesome' }
     ]
-     
   }
-    
+  
 })
+  app4.todos.push({ text: 'New item' })
+  app4.todos.push({ text: 'New item 1' })
